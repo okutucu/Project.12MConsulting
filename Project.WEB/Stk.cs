435980f019b767bcef6f6a6044b362cd3ae99e73
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-12-27 11:05:31 +0300

    Transferred db with db first method

---
+++ Project.WEB/Stk.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project.WEB
+{
+    public partial class Stk
+    {
+        public int Id { get; set; }
+        public string MalKodu { get; set; } = null!;
+        public string MalAdi { get; set; } = null!;
+    }
+}
---
+++ Project.WEB/TestContext.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Project.WEB
+{
+    public partial class TestContext : DbContext
+    {
+        public TestContext()
+        {
+        }
+
+        public TestContext(DbContextOptions<TestContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Sti> Stis { get; set; } = null!;
+        public virtual DbSet<Stk> Stks { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=OGUZ1995\\OGUZ;Database=Test;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sti>(entity =>
+            {
+                entity.HasKey(e => new { e.EvrakNo, e.Tarih, e.IslemTur })
+                    .HasName("pkSTI");
+
+                entity.ToTable("STI");
+
+                entity.Property(e => e.EvrakNo)
+                    .HasMaxLength(16)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Birim)
+                    .HasMaxLength(4)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Fiyat).HasColumnType("numeric(25, 6)");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.MalKodu)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Miktar).HasColumnType("numeric(25, 6)");
+
+                entity.Property(e => e.Tutar).HasColumnType("numeric(25, 6)");
+            });
+
+            modelBuilder.Entity<Stk>(entity =>
+            {
+                entity.HasKey(e => e.MalKodu)
+                    .HasName("pkSTK");
+
+                entity.ToTable("STK");
+
+                entity.Property(e => e.MalKodu)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.MalAdi)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
